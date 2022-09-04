6a15e28ed573f9bcafca634f7bac3232bb37eddd
Nes-cmd <nesrusadik0@gmail.com>
2022-09-04 14:31:57 +0300

    photo attribute prepared for api

--- app/Models/Slider.php
+++ app/Models/Slider.php
@@ -4,10 +4,19 @@ namespace App\Models;
 
 // use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Slider extends Model
 {
     // use HasFactory;
     protected $guarded = [];
-    
+    protected function photoUrl(): Attribute
+    {
+        if(!request()->is('api/*')){
+              return Attribute::make(
+                    get: fn ($value) => json_decode($value)
+            );
+         }
+        return Attribute::make(fn($value) => env('APP_URL').'/storage/'.$value);
+    }
 }
