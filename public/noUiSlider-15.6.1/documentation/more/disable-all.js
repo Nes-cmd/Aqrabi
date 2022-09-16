80f23a0c20aad26706fdc3104a2e152956a03d4c
Nes-cmd <nesrusadik0@gmail.com>
2022-09-16 21:25:01 +0300

    Color removea

--- public/noUiSlider-15.6.1/documentation/more/disable-all.js
+++
@@ -1,9 +0,0 @@
-// To disable
-slider.setAttribute('disabled', true);
-
-// To re-enable
-slider.removeAttribute('disabled');
-
-// To disable one handle
-var origins = slider.querySelectorAll('.noUi-origin');
-origins[0].setAttribute('disabled', true);
